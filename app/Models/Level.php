afa0e83ac8a1ddaac2577127853d16458e4ba035
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 17:00:32 +0100

    feat: add level model

---
+++ app/Models/Level.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\Level
+ *
+ * @property int $id
+ * @property int $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Level whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Level whereName($value)
+ * @mixin \Eloquent
+ */
+class Level extends BaseModel
+{
+
+}
