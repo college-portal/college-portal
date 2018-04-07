64913b49025af95ca22b11cb74b366ff116e7d00
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 15:41:16 +0100

    feat: create models for faculty and school

---
+++ app/Models/Faculty.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\Faculty
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $school_id
+ * @property int $dean_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Faculty whereContains($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Faculty whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Faculty whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Faculty whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Faculty whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Faculty extends BaseModel
+{
+
+}
---
+++ app/Models/School.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\School
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $owner_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\School whereContains($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\School whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\School whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\School whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\School whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class School extends BaseModel
+{
+
+}
