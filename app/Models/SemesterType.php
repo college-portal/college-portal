65cc5c754813e41162f5aaa5f9099c0a4bcb3971
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 17:00:54 +0100

    feat: add semester-type model

---
+++ app/Models/SemesterType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\SemesterType
+ *
+ * @property int $id
+ * @property int $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SemesterType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\SemesterType whereName($value)
+ * @mixin \Eloquent
+ */
+class SemesterType extends BaseModel
+{
+
+}
