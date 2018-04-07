64a265af411a3dc4089d3a4fa8c55a6ac337d34d
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 17:39:04 +0100

    feat: add chargeable service

---
+++ app/Models/Chargeable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\Chargeable
+ *
+ * @property int $id
+ * @property int $chargeable_service_id
+ * @property string $chargeable_id
+ * @property string $chargeable_type
+ * @property float $amount
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Chargeable whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Chargeable whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Chargeable whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Chargeable whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Chargeable extends BaseModel
+{
+
+}
