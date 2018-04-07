6a8bc7176b7a93c05023d268379457498b478f81
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 17:00:17 +0100

    feat: add grade-type model

---
+++ app/Models/GradeType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\GradeType
+ *
+ * @property int $id
+ * @property int $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\GradeType whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\GradeType whereName($value)
+ * @mixin \Eloquent
+ */
+class GradeType extends BaseModel
+{
+
+}
