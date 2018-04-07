6ac9e719e53ee6ceeb1becebd46fe4b3076422ab
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 15:45:04 +0100

    feat: create program model

---
+++ app/Models/Program.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\Program
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $department_id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Program whereContains($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Program whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Program whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Program whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Program whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Program extends BaseModel
+{
+
+}
