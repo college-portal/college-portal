8d4973bd95c6532a0e63fbc9df285beb20a71af7
Ikechi Michael <mykehell123@gmail.com>
2018-04-07 17:38:51 +0100

    feat: add action model

---
+++ app/Models/Action.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+/**
+ * App\Models\Action
+ *
+ * @property int $id
+ * @property string $name
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Action whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Action whereName($value)
+ * @mixin \Eloquent
+ */
+class Action extends BaseModel
+{
+
+}
