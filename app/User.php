2608f9b8d53f03db9d8d47e256a24ab7669c61e6
Ikechi Michael <mykehell123@gmail.com>
2018-04-09 17:34:22 +0100

    feat: db migration create users table #3

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'other_names', 'name', 'email', 'password', 'dob'
+        'first_name', 'last_name', 'other_names', 'email', 'password', 'dob'
     ];
 
     /**
