1f7675207caffb2de34667f967eafeed8f010e9d
Ikechi Michael <mykehell123@gmail.com>
2018-04-16 13:24:18 +0100

    feat: add migration to update relationships in courses table

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -11,7 +11,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class BaseModel extends Model
 {
-    public static function table() {
+    public static function name() {
         return with(new static)->getTable();
     }
 }
