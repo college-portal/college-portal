f6cc88b942718ec5b330e4ca2e22c6f62eac95ed
mykeels <mykehell123@gmail.com>
2018-06-14 13:14:46 +0100

    feat: grade and student_takes_course relationship

---
+++ database/migrations/2018_06_14_120945_rel_grade_type_student.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Grade;
+use App\Models\StudentTakesCourse;
+
+class RelGradeTypeStudent extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(Grade::name(), function (Blueprint $table) {
+            $table->foreign('student_takes_course_id')->references('id')->on(StudentTakesCourse::name());
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(Grade::name(), function (Blueprint $table) {
+            $table->dropColumn([ 'student_takes_course_id' ]);
+        });
+    }
+}
