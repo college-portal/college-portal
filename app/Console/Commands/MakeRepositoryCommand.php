76a4c7ecb0a79299b8a3045b25575338c650944c
Ikechi Michael <mykehell123@gmail.com>
2018-06-20 10:00:04 +0100

    feat: add fill-in methods to repository template

--- app/Console/Commands/MakeRepositoryCommand.php
+++ app/Console/Commands/MakeRepositoryCommand.php
@@ -66,6 +66,31 @@ class MakeRepositoryCommand extends Command
             {
                 return app(${name}::class);
             }
+
+            public function list(User \$user, ${name}Filters \$filters) {
+                return \$this->model->filter(\$filters)->paginate();
+            }
+        
+            public function single(\$id, ${name}Filters \$filters = null) {
+                \$q = \$this->model();
+                if (\$filters) {
+                    \$q = \$q->filter(\$filters);
+                }
+                return \$q->findOrFail(\$id);
+            }
+        
+            public function delete(\$id) {
+                return \$this->model()->where('id', \$id)->delete();
+            }
+        
+            public function create(\$opts) {
+                return \$this->model()->create(\$opts);
+            }
+        
+            public function update(\$id, \$opts = []) {
+                \$this->model()->where('id', \$id)->update(\$opts);
+                return \$this->single(\$id);
+            }
         
             public function count()
             {
