7ceb8a491d169926eb20d8397742bce2d73f27e9
Ikechi Michael <mykehell123@gmail.com>
2018-06-23 17:21:12 +0100

    doc: comments on ChargeableService

--- app/Services/ChargeableService.php
+++ app/Services/ChargeableService.php
@@ -6,6 +6,9 @@ use App\Repositories\ChargeableRepository;
 use App\Repositories\ChargeableServiceRepository;
 use Carbon\Carbon;
 
+/**
+ * A service class for two repositories
+ */
 class ChargeableService
 {
     public function repo()
@@ -18,15 +21,44 @@ class ChargeableService
         return app(ChargeableServiceRepository::class);
     }
 
+    /**
+     * attempts to create a new Chargeable
+     * 
+     * @param array $opts
+     * @param integer $opts->chargeable_service_id
+     * @param float $opts->amount
+     * 
+     * @return \App\Models\Chargeable
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
     public function create($opts) {
         $chargeable_service_id = $opts['chargeable_service_id'];
 
         $service = $this->serviceRepo()->single($chargeable_service_id);
 
+        /**
+         * it's not important to authorize the user for the chargeable-service here, as 
+         * this has already been taken care of in the App\Http\Requests\ChargeableRequest class
+         */
+
         return $this->repo()->create(array_merge([ 'amount' => $service->amount ], $opts));
     }
 
-    public function update($opts) {
+    /**
+     * attempts to update the details of a Chargeable
+     * 
+     * @param integer $id
+     * @param array $opts
+     * @param integer $opts->chargeable_service_id
+     * @param integer $opts->chargeable_id
+     * @param float $opts->amount
+     * 
+     * @return \App\Models\Chargeable
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update($id, $opts) {
         $user = auth()->user()->first();
         $chargeable_id = isset($opts['chargeable_id']) ? $opts['chargeable_id'] : null;
         $chargeable_service_id = isset($opts['chargeable_service_id']) ? $opts['chargeable_service_id'] : null;
@@ -43,6 +75,6 @@ class ChargeableService
             $user->authorize('update', $owner);
         }
 
-        return $this->repo()->update($opts);
+        return $this->repo()->update($id, $opts);
     }
 }
