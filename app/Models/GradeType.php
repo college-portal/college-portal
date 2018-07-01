7f50f21291325b4f8332c85c2cea691b88e7dbf4
Ikechi Michael <mykehell123@gmail.com>
2018-07-01 10:05:48 +0100

    feat: create grade_types table

--- app/Models/GradeType.php
+++ app/Models/GradeType.php
@@ -3,17 +3,28 @@
 namespace App\Models;
 
 use App\Models\BaseModel;
+use App\Models\School;
 
 /**
  * App\Models\GradeType
  *
  * @property int $id
- * @property int $name
+ * @property string $name
+ * @property int $value
+ * @property int $school_id
+ * @property int $minimum
+ * @property int $maximum
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\GradeType whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\GradeType whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Grade whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class GradeType extends BaseModel
 {
-
+    public function school() {
+        return $this->belongsTo(School::class);
+    }
 }
