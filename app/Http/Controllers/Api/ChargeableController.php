643aada39f63e7473a79e3a751425f444b8a235b
Ikechi Michael <mykehell123@gmail.com>
2018-07-04 12:47:15 +0100

    doc: chargeable controller comments

--- app/Http/Controllers/Api/ChargeableController.php
+++ app/Http/Controllers/Api/ChargeableController.php
@@ -19,17 +19,34 @@ class ChargeableController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Chargeable by ID
+     * 
+     * Responds with a specific Chargeable by its ID
+     * 
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function show(Request $request, ChargeableFilters $filters, int $id) {
         $chargeable = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $chargeable); /** ensure the current user has view rights */
         return $chargeable;
     }
 
+    /**
+     * Gets Chargeables List
+     * 
+     * Responds with a list of Chargeables the user has access to
+     */
     public function index(Request $request, ChargeableFilters $filters) {
         $chargeables = $this->service()->repo()->list($request->user(), $filters);
         return $chargeables;
     }
 
+    /**
+     * Delete Chargeable
+     * 
+     * Removes a Chargeable from the System by ID
+     */
     public function destroy(Request $request, int $id) {
         $chargeable = $this->service()->repo()->single($id);
         $this->authorize('delete', $chargeable); /** ensure the current user has delete rights */
@@ -37,11 +54,21 @@ class ChargeableController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Chargeable
+     * 
+     * Supply chargeable information to create a new one
+     */
     public function store(ChargeableRequest $request) {
         $chargeable = $this->service()->create($request->all());
         return $this->json($chargeable);
     }
 
+    /**
+     * Update Chargeable
+     * 
+     * Modify information about an existing chargeable by ID
+     */
     public function update(Request $request, int $id) {
         $chargeable = $this->service()->repo()->single($id);
         $this->authorize('update', $chargeable);
