3cc9b87ea7e606514dec05826dcf85c649a771dc
Ikechi Michael <mykehell123@gmail.com>
2018-07-08 10:44:19 +0100

    feat: move gh-deploy commands to a *.sh file

---
+++ gh-deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git checkout edge
+rm -rf public/docs
+read -s -p "Enter Password: " ADMIN_PASSWORD
+php artisan generate:docs --password=$ADMIN_PASSWORD
+git branch -D gh-pages
+git subtree split --prefix public/docs -b gh-pages
+touch public/docs/.gitignore
+grep -v "public/docs" .gitignore >> public/docs/.gitignore
+git checkout gh-pages
+mv public/docs/.gitignore .gitignore
+git add .
+git commit -m "chore: generate gh-pages"
+git push -d origin gh-pages
+git subtree push --prefix public/docs origin gh-pages
+git checkout edge
