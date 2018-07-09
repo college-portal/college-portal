944587141e13bb8b9204e88ece82f698686eaee2
Ikechi Michael <mykehell123@gmail.com>
2018-07-09 15:15:59 +0100

    test: course-dependencies

--- app/Services/CourseDependencyService.php
+++ app/Services/CourseDependencyService.php
@@ -3,10 +3,18 @@
 namespace App\Services;
 
 use App\Repositories\CourseDependencyRepository;
+use App\Repositories\CourseRepository;
+use Illuminate\Validation\ValidationException;
 use Carbon\Carbon;
 
 class CourseDependencyService
 {
+    private $courseRepository;
+
+    public function __construct(CourseRepository $courseRepository) {
+        $this->courseRepository = $courseRepository;
+    }
+
     public function repo()
     {
         return app(CourseDependencyRepository::class);
@@ -14,12 +22,33 @@ class CourseDependencyService
 
     public function update($id, $opts = []) {
         request()->validate([
-            'course_id' => 'numeric|exists:courses,id|different:dependency_id',
-            'dependency_id' => 'numeric|exists:courses,id|different:course_id'
+            'course_id' => 'numeric|exists:courses,id',
+            'dependency_id' => 'numeric|exists:courses,id'
         ]);
+
+        $course_id = isset($opts['course_id']) ? $opts['course_id'] : null;
+        $dependency_id = isset($opts['dependency_id']) ? $opts['dependency_id'] : null;
+
+        if ($course_id && $dependency_id && ($course_id == $dependency_id)) {
+            throw ValidationException::withMessages([
+                'course_id' => 'must be different from dependency_id',
+                'dependency_id' => 'must be different from course_id'
+            ]);
+        }
+
         $user = auth()->user()->first();
-        $dependency = $this->repo()->single($id);
-        $user->authorize('update', $dependency);
+        if ($course_id) {
+            $course = $this->courseRepository->course($course_id);
+            $user->authorize('update', $course);
+        }
+        if ($dependency_id) {
+            $dependency = $this->courseRepository->course($dependency_id);
+            $user->authorize('update', $dependency);
+        }
+
+        $courseDependency = $this->repo()->single($id);
+        $user->authorize('update', $courseDependency);
+
         return $this->repo()->update($id, $opts);
     }
 }
---
+++ tests/Feature/CourseDependencyTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CourseDependencyTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * /api/course-dependencies GET
+     * 
+     * should return a list of CourseDependencies
+     *
+     * @return void
+     */
+    public function testGetCourseDependencyList()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->get($this->url('course-dependencies'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    'course_id',
+                    'dependency_id'
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * /api/course-dependencies/1 GET
+     * 
+     * should return a CourseDependency
+     *
+     * @return void
+     */
+    public function testGetCourseDependency()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->get($this->url('course-dependencies/1'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'course_id',
+            'dependency_id'
+        ]);
+    }
+    
+    /**
+     * /api/course-dependencies POST
+     * 
+     * should create a new CourseDependency
+     *
+     * @return void
+     */
+    public function testCreateCourseDependency()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->post($this->url('course-dependencies'), [
+                            'course_id' => 8,
+                            'dependency_id' => 7
+                        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonStructure([
+            'course_id',
+            'dependency_id'
+        ]);
+
+        $response->assertJson([
+            'course_id' => 8
+        ]);
+    }
+    
+    /**
+     * /api/course-dependencies/1 PUT
+     * 
+     * should modify the CourseDependency's name
+     *
+     * @return void
+     */
+    public function testUpdateCourseDependency()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->put($this->url('course-dependencies/1'), [
+                            'course_id' => 5
+                        ]);
+                        
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'course_id',
+            'dependency_id'
+        ]);
+
+        $response->assertJson([
+            'course_id' => 5
+        ]);
+    }
+    
+    /**
+     * /api/course-dependencies/1 DELETE
+     * 
+     * should delete the CourseDependency
+     *
+     * @return void
+     */
+    public function testDeleteCourseDependency()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->delete($this->url('course-dependencies/1'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'message'
+        ]);
+    }
+}
