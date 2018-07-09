369932c54ae25de6186afce98d614570a7643d90
Ikechi Michael <mykehell123@gmail.com>
2018-07-09 09:40:08 +0100

    test: program

---
+++ tests/Feature/ProgramTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ProgramTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * /api/programs GET
+     * 
+     * should return a list of Programs
+     *
+     * @return void
+     */
+    public function testGetProgramList()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->get($this->url('programs'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'name',
+                    'department_id'
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * /api/programs/1 GET
+     * 
+     * should return a Program
+     *
+     * @return void
+     */
+    public function testGetProgram()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->get($this->url('programs/1'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'id',
+            'name',
+            'department_id'
+        ]);
+    }
+    
+    /**
+     * /api/programs POST
+     * 
+     * should create a new Program
+     *
+     * @return void
+     */
+    public function testCreateProgram()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->post($this->url('programs'), [
+                            'name' => 'Physical Science',
+                            'department_id' => 1
+                        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonStructure([
+            'id',
+            'name',
+            'department_id'
+        ]);
+
+        $response->assertJson([
+            'name' => 'Physical Science'
+        ]);
+    }
+    
+    /**
+     * /api/programs/1 PUT
+     * 
+     * should modify the Program's name
+     *
+     * @return void
+     */
+    public function testUpdateProgram()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->put($this->url('programs/1'), [
+                            'name' => 'Information Systems'
+                        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'id',
+            'name',
+            'department_id'
+        ]);
+
+        $response->assertJson([
+            'name' => 'Information Systems'
+        ]);
+    }
+    
+    /**
+     * /api/programs/1 DELETE
+     * 
+     * should delete the Program
+     *
+     * @return void
+     */
+    public function testDeleteProgram()
+    {
+        $response = $this->loginAsRole(Role::ADMIN)
+                        ->delete($this->url('programs/1'));
+
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'message'
+        ]);
+    }
+}
