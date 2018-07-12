25e9de4b8a4c8aa040df612776e98e1bb8519cf3
Ikechi Michael <mykehell123@gmail.com>
2018-07-12 15:20:36 +0100

    feat: move auth logic to service and repository

--- app/Http/Controllers/AuthGoogleController.php
+++ app/Http/Controllers/AuthGoogleController.php
@@ -5,12 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Services\UserService;
 use App\User;
 use Auth;
 use Socialite;
 
 class AuthGoogleController extends Controller
 {
+    protected $service;
+
+    public function __construct(UserService $service) {
+        $this->service = $service;
+    }
+
+    public function service() {
+        return $this->service;
+    }
 
     public function redirect()
     {
@@ -20,30 +30,16 @@ class AuthGoogleController extends Controller
 
     public function callback()
     {
-        try {
-            $googleUser = Socialite::driver('google')->user();
-            $existUser = User::where('email',$googleUser->email)->first();
-            
+        $googleUser = Socialite::driver('google')->user();
+        $existingUser = $this->service()->repo()->userByEmail($googleUser->email);
 
-            if($existUser) {
-                Auth::loginUsingId($existUser->id);
-            }
-            else {
-                $user = new User;
-                $names = explode(' ', $googleUser->name);
-                if (isset($names[0])) $user->first_name = $names[0];
-                if (isset($names[1])) $user->last_name = $names[1];
-                if (isset($names[2])) $user->other_names = $names[2];
-                $user->email = $googleUser->email;
-                $user->google_id = $googleUser->id;
-                $user->password = md5(rand(1,10000));
-                $user->save();
-                Auth::loginUsingId($user->id);
-            }
-            return redirect()->to('/home');
-        } 
-        catch (Exception $e) {
-            return 'error';
+        if($existingUser) {
+            Auth::loginUsingId($existingUser->id);
         }
+        else {
+            $user = $this->service()->createFromGoogle($googleUser);
+            Auth::loginUsingId($user->id);
+        }
+        return redirect()->to('/home');
     }
 }
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\User;
 use App\Repositories\UserRepository;
 use Carbon\Carbon;
 
@@ -10,5 +11,22 @@ class UserService extends BaseService
     public function repo()
     {
         return app(UserRepository::class);
+    }
+
+    public function createFromGoogle($googleUser) {
+        $user = new User();
+
+        $names = explode(' ', $googleUser->name);
+        if (isset($names[0])) $user->first_name = $names[0];
+        if (isset($names[1])) $user->last_name = $names[1];
+        if (isset($names[2])) $user->other_names = $names[2];
+
+        $user->email = $googleUser->email;
+        $user->google_id = $googleUser->id;
+        $user->password = md5(rand(1,10000));
+
+        $user->save();
+
+        return $user;
     }
 }
