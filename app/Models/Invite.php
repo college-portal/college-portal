b3587506eec00bbb74798a189b5266cbaabcf2b1
Ikechi Michael <mykehell123@gmail.com>
2018-07-26 09:27:17 +0100

    feat: created invite and invite-role models

---
+++ app/Models/Invite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use App\Models\InviteRole;
+
+/**
+ * App\Models\Invite
+ *
+ * @property int $id
+ * @property int $creator_id
+ * @property int $school_id
+ * @property int $email
+ * @property string $message
+ * @property \Carbon\Carbon $accepted_at
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Invite whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Invite whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Invite whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Invite extends BaseModel
+{
+
+    protected $fillable = [ 'creator_id', 'school_id', 'email', 'message' ];
+
+    public function roles() {
+        return $this->hasMany(InviteRole::class);
+    }
+
+    public function school() {
+        return $this->belongsTo(School::class);
+    }
+}
---
+++ app/Models/InviteRole.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use App\Models\Invite;
+
+/**
+ * App\Models\InviteRole
+ *
+ * @property int $id
+ * @property int $invite_id
+ * @property int $role_id
+ * @property string $extras
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\InviteRole whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\InviteRole whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\InviteRole whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class InviteRole extends BaseModel
+{
+
+    protected $fillable = [ 'invite_id', 'role_id', 'extras' ];
+
+    public function invite() {
+        return $this->belongsTo(Invite::class);
+    }
+
+    public function scopeSchool() {
+        $ids = $this->invite()->pluck('school_id');
+        return School::whereIn('id', $ids);
+    }
+
+    public function setExtrasAttribute($value) {
+        if (!is_string($value)) {
+            $this->attributes['extras'] = json_encode($value);
+        }
+        else {
+            $this->attributes['extras'] = $value;
+        }
+    }
+}
