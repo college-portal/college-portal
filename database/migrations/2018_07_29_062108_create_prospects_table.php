72802a66317b1efac2314eb5285d4ab6b88a705e
Ikechi Michael <mykehell123@gmail.com>
2018-07-29 07:38:27 +0100

    feat: prospect migrations

---
+++ database/migrations/2018_07_29_062108_create_prospects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use App\Models\Prospect;
+
+class CreateProspectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(Prospect::name(), function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('school_id')->unsigned();
+            $table->integer('program_id')->unsigned();
+            $table->integer('session_id')->unsigned();
+            $table->timestamp('locked_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(Prospect::name());
+    }
+}
