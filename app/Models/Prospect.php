83de54422affde9db9e13100d49277e84c8c0045
Ikechi Michael <mykehell123@gmail.com>
2018-07-29 07:38:19 +0100

    feat: addition prospect properties and relationships

--- app/Models/Prospect.php
+++ app/Models/Prospect.php
@@ -5,14 +5,24 @@ namespace App\Models;
 use App\User;
 use App\Models\School;
 use App\Models\Program;
+use App\Models\Session;
 use App\Models\BaseModel;
 
 /**
  * App\Models\Prospect
+ * 
+ * A prospect is a user who is an admission candidate to a particular school,
+ *  in a particular program and targeting a particular session.
+ * 
+ * A prospect can be locked for editing by a school-owner, similar to closing the 
+ *  admission process.
  *
  * @property int $id
+ * @property int $user_id
  * @property int $school_id
  * @property int $program_id
+ * @property int $session_id
+ * @property \Carbon\Carbon $locked_at
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Prospect whereId($value)
@@ -24,7 +34,11 @@ use App\Models\BaseModel;
  */
 class Prospect extends BaseModel
 {
-    protected $fillable = [ 'school_id', 'program_id' ];
+    protected $fillable = [ 'user_id', 'school_id', 'program_id', 'session_id' ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 
     public function school() {
         return $this->belongsTo(School::class);
@@ -32,6 +46,10 @@ class Prospect extends BaseModel
 
     public function program() {
         return $this->belongsTo(Program::class);
+    }
+
+    public function session() {
+        return $this->belongsTo(Session::class);
     }
 
     public static function boot() {
