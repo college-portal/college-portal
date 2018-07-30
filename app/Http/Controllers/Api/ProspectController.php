2ce4a4009eb65604cde84c185fbe8c5977b78335
Ikechi Michael <mykehell123@gmail.com>
2018-07-30 09:45:13 +0100

    doc: prospect controller comments

--- app/Http/Controllers/Api/ProspectController.php
+++ app/Http/Controllers/Api/ProspectController.php
@@ -19,17 +19,39 @@ class ProspectController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Prospect by ID
+     * 
+     * Responds with a specific Prospect by its ID
+     * - Rules of Access
+     *   - User is an ADMIN or SCHOOL_OWNER of prospect's school
+     */
     public function show(Request $request, ProspectFilters $filters, $id) {
         $prospect = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $prospect); /** ensure the current user has view rights */
         return $prospect;
     }
 
+    /**
+     * Get Prospects
+     * 
+     * Responds with a list of Prospects
+     * - Rules of Access
+     *   - User is same school as prospect
+     */
     public function index(Request $request, ProspectFilters $filters) {
         $prospects = $this->service()->repo()->list($request->user(), $filters);
         return $prospects;
     }
 
+    /**
+     * Delete Prospect
+     * 
+     * Removes a Prospect from the System by ID
+     * - Rules of Access
+     *  - User is an ADMIN or
+     *  - User owns school the Prospect belongs to
+     */
     public function destroy(Request $request, $id) {
         $prospect = $this->service()->repo()->single($id);
         $this->authorize('delete', $prospect); /** ensure the current user has delete rights */
@@ -37,11 +59,27 @@ class ProspectController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Prospect
+     * 
+     * Supply Prospect information to create a new one
+     * - Rules of Access
+     *  - User is an ADMIN or
+     *  - User owns school the Program belongs to
+     */
     public function store(ProspectRequest $request) {
         $prospect = $this->service()->repo()->create(array_merge($request->all(), [ 'user_id' => $request->user()->id ]));
         return $this->created($prospect);
     }
 
+    /**
+     * Update Prospect
+     * 
+     * Modify information about an existing Program by ID
+     * - Rules of Access
+     *  - User is an ADMIN or
+     *  - User owns school the Program belongs to
+     */
     public function update(Request $request, $id) {
         $prospect = $this->service()->repo()->single($id);
         $this->authorize('update', $prospect);
