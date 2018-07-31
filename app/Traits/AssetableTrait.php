bf6325ff4f8cdfc29e0cb59c04d2accf32c425a8
Ikechi Michael <mykehell123@gmail.com>
2018-07-31 14:53:13 +0100

    feat: use static::class rather than self::class

--- app/Traits/AssetableTrait.php
+++ app/Traits/AssetableTrait.php
@@ -8,7 +8,7 @@ trait AssetableTrait
 {
     public function assets() {
         return $this->hasMany(Asset::class, 'owner_id')->whereHas('type', function ($q) {
-            return $q->where('type', self::class);
+            return $q->where('type', static::class);
         });
     }
 }
--- app/Traits/ImageableTrait.php
+++ app/Traits/ImageableTrait.php
@@ -8,7 +8,7 @@ trait ImageableTrait
 {
     public function images() {
         return $this->hasMany(Image::class, 'owner_id')->whereHas('type', function ($q) {
-            return $q->where('type', self::class);
+            return $q->where('type', static::class);
         });
     }
 }
