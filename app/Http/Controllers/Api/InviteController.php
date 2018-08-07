0bb172a7546a2e50d6537450b114b5352714f052
Ikechi Michael <mykehell123@gmail.com>
2018-08-07 09:23:33 +0100

    doc: add comments to invite controller

--- app/Http/Controllers/Api/InviteController.php
+++ app/Http/Controllers/Api/InviteController.php
@@ -19,17 +19,47 @@ class InviteController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Invite by ID
+     * 
+     * Responds with a specific Invite by its ID
+     * - Rules of Access
+     *   - User is an ADMIN
+     *   - User owns school where Invite is made to
+     *   - User has role of INVITE_CREATOR in school where Invite is made to
+     *   - User created the Invite
+     */
     public function show(Request $request, InviteFilters $filters, $id) {
         $invite = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $invite); /** ensure the current user has view rights */
         return $invite;
     }
 
+    /**
+     * Get Invites
+     * 
+     * Responds with a list of Invites the user has access to view
+     * - Rules of Access
+     *   - User is an ADMIN
+     *   - User owns school where Invite is made to
+     *   - User has role of INVITE_CREATOR in school where Invite is made to
+     *   - User created the Invite
+     */
     public function index(Request $request, InviteFilters $filters) {
         $invites = $this->service()->repo()->list($request->user(), $filters);
         return $invites;
     }
 
+    /**
+     * Delete Invite
+     * 
+     * Removes a Invite from the System by ID
+     * - Rules of Access
+     *   - User is an ADMIN
+     *   - User owns school where Invite is made to
+     *   - User has role of INVITE_CREATOR in school where Invite is made to
+     *   - User created the Invite
+     */
     public function destroy(Request $request, $id) {
         $invite = $this->service()->repo()->single($id);
         $this->authorize('delete', $invite); /** ensure the current user has delete rights */
@@ -37,11 +67,27 @@ class InviteController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Invite
+     * 
+     * Supply Invite information to create a new one
+     * - Rules of Access
+     *  - User can invite an ADMIN only if User is an ADMIN
+     *  - User can invite a SCHOOL_OWNER if User is an ADMIN or SCHOOL_OWNER in same school
+     *  - User can invite anyone else to a school if User is ADMIN / SCHOOL_OWNER / INVITE_CREATOR
+     */
     public function store(InviteRequest $request) {
         $invite = $this->service()->create(array_merge([ 'creator_id' => $request->user()->id ], $request->all()));
         return $this->created($invite);
     }
 
+    /**
+     * Update Invite
+     * 
+     * Modify information about an existing Invite by ID
+     * - Rules of Access
+     *  - Invite was made to User's email address
+     */
     public function update(Request $request, $id) {
         $invite = $this->service()->repo()->single($id);
         $this->authorize('update', $invite);
