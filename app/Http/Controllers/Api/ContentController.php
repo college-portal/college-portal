11daa5c64d1e090c63b4a03831f6add12ee12109
Ikechi Michael <mykehell123@gmail.com>
2018-08-07 09:23:25 +0100

    doc: add comments to content controller

--- app/Http/Controllers/Api/ContentController.php
+++ app/Http/Controllers/Api/ContentController.php
@@ -19,17 +19,36 @@ class ContentController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Content by ID
+     * 
+     * Responds with a specific Content by its ID
+     * - Rules of Access
+     *   - User is in the same school as the user who owns the content
+     */
     public function show(Request $request, ContentFilters $filters, $id) {
         $content = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $content); /** ensure the current user has view rights */
         return $content;
     }
 
+    /**
+     * Get Contents
+     * 
+     * Responds with a list of Content
+     */
     public function index(Request $request, ContentFilters $filters) {
         $contents = $this->service()->repo()->list($request->user(), $filters);
         return $contents;
     }
 
+    /**
+     * Delete Content
+     * 
+     * Removes a Content from the System by ID
+     * - Rules of Access
+     *  - User can update the model the Content belongs to
+     */
     public function destroy(Request $request, $id) {
         $content = $this->service()->repo()->single($id);
         $this->authorize('delete', $content); /** ensure the current user has delete rights */
@@ -37,11 +56,25 @@ class ContentController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Content
+     * 
+     * Supply Content information to create a new one
+     * - Rules of Access
+     *  - User can update the model the Content belongs to
+     */
     public function store(ContentRequest $request) {
         $content = $this->service()->create($request->all());
         return $this->created($content);
     }
 
+    /**
+     * Update Content
+     * 
+     * Modify information about an existing Content by ID
+     * - Rules of Access
+     *  - User can update the model the Content belongs to
+     */
     public function update(Request $request, $id) {
         $grade = $this->service()->repo()->single($id);
         $this->authorize('update', $grade);
