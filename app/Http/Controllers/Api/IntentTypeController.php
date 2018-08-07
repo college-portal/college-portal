534a6d5ed75727699f06695159e9339910533541
Ikechi Michael <mykehell123@gmail.com>
2018-08-07 09:42:09 +0100

    doc: add comments in intent-type controller

--- app/Http/Controllers/Api/IntentTypeController.php
+++ app/Http/Controllers/Api/IntentTypeController.php
@@ -19,17 +19,34 @@ class IntentTypeController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Intent Type by ID
+     * 
+     * Responds with a specific Intent Type by its ID
+     */
     public function show(Request $request, IntentTypeFilters $filters, $id) {
         $type = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $type); /** ensure the current user has view rights */
         return $type;
     }
 
+    /**
+     * Get Intent Types
+     * 
+     * Responds with a list of Intent Types
+     */
     public function index(Request $request, IntentTypeFilters $filters) {
         $types = $this->service()->repo()->list($request->user(), $filters);
         return $types;
     }
 
+    /**
+     * Delete Intent Type
+     * 
+     * Removes an Intent Type from the System by ID
+     * - Rules of Access
+     *  - User is an ADMIN
+     */
     public function destroy(Request $request, $id) {
         $type = $this->service()->repo()->single($id);
         $this->authorize('delete', $type); /** ensure the current user has delete rights */
@@ -37,11 +54,25 @@ class IntentTypeController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Intent Type
+     * 
+     * Supply Intent Type information to create a new one
+     * - Rules of Access
+     *  - User is an ADMIN
+     */
     public function store(IntentTypeRequest $request) {
         $type = $this->service()->repo()->create($request->all());
         return $this->created($type);
     }
 
+    /**
+     * Update Intent Type
+     * 
+     * Modifies information on an existing Intent Type
+     * - Rules of Access
+     *  - User is an ADMIN
+     */
     public function update(Request $request, $id) {
         $type = $this->service()->repo()->single($id);
         $this->authorize('update', $type);
