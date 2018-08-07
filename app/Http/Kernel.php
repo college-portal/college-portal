12219467eb234143d314bae940959491a2c1435d
Ikechi Michael <mykehell123@gmail.com>
2018-08-07 08:38:12 +0100

    feat: use transaction middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             // 'throttle:60,1',
             'bindings',
+            'useTransaction'
         ],
     ];
 
@@ -59,6 +60,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'accessTokenSecurity' => \App\Http\Middleware\AccessTokenSecurity::class
+        'accessTokenSecurity' => \App\Http\Middleware\AccessTokenSecurity::class,
+        'useTransaction' => \App\Http\Middleware\UseTransaction::class
     ];
 }
---
+++ app/Http/Middleware/UseTransaction.php
@@ -0,0 +1,39 @@
+<?php namespace App\Http\Middleware;
+
+use App\User;
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * makes sure the "db:level" header is present
+ * and that it contains "transaction"
+ */
+
+class UseTransaction
+{
+
+    protected $auth;
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function handle(Request $request, Closure $next) {
+        if ($request->header('db') == 'transaction') {
+            DB::beginTransaction();
+        }
+        $response = $next($request);
+        if ($request->header('db') == 'transaction') {
+            DB::rollback();
+        }
+        return $response;
+    }
+}
