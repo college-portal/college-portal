fb5aafed679be5260ce1587609f730411fc4ff20
Ikechi Michael <mykehell123@gmail.com>
2018-08-07 09:39:16 +0100

    doc: add comments in intent controller

--- app/Http/Controllers/Api/IntentController.php
+++ app/Http/Controllers/Api/IntentController.php
@@ -19,17 +19,38 @@ class IntentController extends ApiController
         return $this->service;
     }
 
+    /**
+     * Get Intent by ID
+     * 
+     * Responds with a specific Intent by its ID
+     * - Rules of Access
+     *   - User owns Intent
+     */
     public function show(Request $request, IntentFilters $filters, $id) {
         $intent = $this->service()->repo()->single($id, $filters);
         $this->authorize('view', $intent); /** ensure the current user has view rights */
         return $intent;
     }
 
+    /**
+     * Get Intent by ID
+     * 
+     * Responds with a list of Intents
+     * - Rules of Access
+     *   - User owns Intents
+     */
     public function index(Request $request, IntentFilters $filters) {
         $intents = $this->service()->repo()->list($request->user(), $filters);
         return $intents;
     }
 
+    /**
+     * Delete Intent
+     * 
+     * Removes an Intent from the System by ID
+     * - Rules of Access
+     *  - User owns Intent
+     */
     public function destroy(Request $request, $id) {
         $intent = $this->service()->repo()->single($id);
         $this->authorize('delete', $intent); /** ensure the current user has delete rights */
@@ -37,11 +58,19 @@ class IntentController extends ApiController
         return $this->ok();
     }
 
+    /**
+     * Create Intent
+     * 
+     * Supply Intent information to create a new one
+     */
     public function store(IntentRequest $request) {
         $intent = $this->service()->create(array_merge([ 'user_id' => $request->user()->id ], $request->all()));
         return $this->created($intent);
     }
 
+    /**
+     * Update Intent (Not Supported)
+     */
     public function update(Request $request, $id) {
         return $this->json([ 'message' => 'This action is to be handled by the System' ], 501);
     }
