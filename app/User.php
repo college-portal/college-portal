7d92631e23540f754d3d1be368c0ecfc9949d68c
Ikechi Michael <mykehell123@gmail.com>
2018-08-12 17:37:36 +0100

    feat: place warning in user.php class 😂

--- app/User.php
+++ app/User.php
@@ -4,6 +4,11 @@ namespace App;
 
 use CollegePortal\Models\User as BaseUser;
 
+/**
+ * DO NOT EDIT, UNLESS YOU MUST!!!
+ * 
+ * SERIOUSLY, DON'T cos it'll have no meaning.
+ */
 class User extends BaseUser
 {
     
